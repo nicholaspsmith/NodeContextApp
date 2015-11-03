fe69798745e48172292fe3070a38112046b2d216
Nicholas Smith <npsmith1990@gmail.com>
2015-11-03 12:32:16 -0600

    authenticate context.io

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var http = require('http');
+var ContextIO = require('contextio');
+var ctxCfg = require('./contextConfig');
+
+var ctxioClient = new ContextIO.Client({
+  key: ctxCfg.key,
+  secret: ctxCfg.secret
+});
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
